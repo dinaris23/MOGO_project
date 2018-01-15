221279c6a305470c7f984172faadcfd518f3050d
Oleksandr58 <kovaliukoleksandr@gmail.com>
2018-01-15 11:11:38 +0200

    Add title, wide container and js to job section

--- app/js/layout/job.js
+++ app/js/layout/job.js
@@ -1,21 +1,11 @@
-/*
-$('.popups__popup').click(function() {
-    if ($(this).next().css('display') == 'block') {
-        return 0;
-    }
-
-    for (var i = 0; i < $('.popups__popup').length; i++) {
-        if ($('.popups__popup')[i].next().css('display') != 'block') {
-            $('.popups__popup')[i].next().slideToggle(1000);
-        }
-    }
-    $(this).next().slideToggle(1000);
- }); */
-
  $('.popups__popup').click(function() {
     if ($(this).next().css('display') == 'block') {
+        $('.svg').removeClass('svg');
+        $('.js-active').slideToggle(1000).removeClass('js-active');
         return 0;
     }
+    $('.svg').removeClass('svg');
+    $(this).children('.svg-icon-grey').addClass('svg');
     $('.js-active').slideToggle(1000).removeClass('js-active');
     $(this).next().slideToggle(1000).addClass('js-active');
  });
