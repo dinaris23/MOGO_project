57159f4931301897b43ee9d3a5a3f8377a74b93f
Oleksandr58 <kovaliukoleksandr@gmail.com>
2018-01-16 18:17:57 +0200

    Adaptive to blog section

--- app/js/layout/feature.js
+++ app/js/layout/feature.js
@@ -1,3 +1,13 @@
 function onIphone () {
     $('.feature__display').toggleClass('noopacity');
 }
+
+$(window).scroll(function() {
+    $('.feature__display').each(function(){
+      var imagePos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+400) {
+        $('.feature__display').addClass('noopacity');
+      }
+    });
+  });
