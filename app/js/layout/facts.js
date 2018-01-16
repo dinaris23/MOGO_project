7a79efb9fe43d26fc7d7d70faa1eb63d31ca4a1e
Illia <ilijaslb@gmail.com>
2018-01-16 15:08:35 +0200

    Fix counter in facts

--- app/js/layout/facts.js
+++ app/js/layout/facts.js
@@ -1,22 +1,29 @@
-$('.fact__counter').each(function() {
-    var $this = $(this),
-        countTo = $this.attr('data-count');
-    
-    $({ countNum: $this.text()}).animate({
-      countNum: countTo
-    },
-  
-    {
-  
-      duration: 1500,
-      easing:'linear',
-      step: function() {
-        $this.text(Math.floor(this.countNum));
+$(window).scroll(function() {
+  var hT = $('.fact__counter').offset().top,
+      hH = $('.fact__counter').outerHeight(),
+      wH = $(window).height(),
+      wS = $(this).scrollTop();
+   console.log((hT-wH) , wS);
+  if (wS > (hT+hH-wH)){
+    $('.fact__counter').each(function() {
+      var $this = $(this),
+          countTo = $this.attr('data-count');
+      $({ countNum: $this.text()}).animate({
+        countNum: countTo
       },
-      complete: function() {
-        $this.text(this.countNum);
-      }
+      {
+        duration: 1500,
+        easing:'linear',
+        step: function() {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function() {
+          $this.text(this.countNum);
+        }
+  
+      });  
+  
+    });
+  }
+});
 
-    });  
-
-  });
